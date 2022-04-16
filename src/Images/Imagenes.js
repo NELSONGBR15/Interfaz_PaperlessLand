d1e2998744bbb68f7fe0a9c6abfb77d7312672e2
NelsonBR <nelson.bocanegra@campusvirtual.aunarvillavicencio.edu.co>
2022-04-15 19:19:21 -0500

    Adelando interfaz Header

---
+++ src/Images/Imagenes.js
@@ -0,0 +1,10 @@
+import logo_palmera from '../Images/Logo_Palmeras_del_llano.png';
+import logo_paperlessland from '../Images/Logo_PapelessLand.png';
+
+
+
+
+export default{
+    "LogoPLL" : logo_palmera,
+    "LogoPL" : logo_paperlessland
+}
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Imagenes from "../Images/Imagenes";
+import '../Styles/Header.css'
+
+export default class Header extends Component{
+    
+render(){
+    return(
+        <header>
+                <div className='logo_palmera'>   <img src={Imagenes.LogoPLL }/>  </div>
+                <div className="LogoPaper" > <img src={Imagenes.LogoPL} />  </div>
+    
+        </header>      
+        )
+    }
+}
